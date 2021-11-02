735d7ea1d6ae5f44381fa6d2a0ae4a33e96bb898
Pavel Kirillov <levru333@gmail.com>
2021-11-02 15:02:54 +0300

    build

--- debian/postinst.sh
+++ debian/postinst.sh
@@ -1,15 +1,15 @@
 if [ "$1" = "configure" ] || [ "$1" = "abort-upgrade" ] || [ "$1" = "abort-deconfigure" ] || [ "$1" = "abort-remove" ] ; then
         # This will only remove masks created by d-s-h on package removal.
-        deb-systemd-helper unmask 'consul-etcddiscovery.service' >/dev/null || true
+        deb-systemd-helper unmask 'coredhcp.service' >/dev/null || true
 
         # was-enabled defaults to true, so new installations run enable.
-        if deb-systemd-helper --quiet was-enabled 'consul-etcddiscovery.service'; then
+        if deb-systemd-helper --quiet was-enabled 'coredhcp.service'; then
                 # Enables the unit on first installation, creates new
                 # symlinks on upgrades if the unit file has changed.
-                deb-systemd-helper enable 'consul-etcddiscovery.service' >/dev/null || true
+                deb-systemd-helper enable 'coredhcp.service' >/dev/null || true
         else
                 # Update the statefile to add new symlinks (if any), which need to be
                 # cleaned up on purge. Also remove old symlinks.
-                deb-systemd-helper update-state 'consul-etcddiscovery.service' >/dev/null || true
+                deb-systemd-helper update-state 'coredhcp.service' >/dev/null || true
         fi
 fi
--- debian/postrm.sh
+++ debian/postrm.sh
@@ -1,12 +1,12 @@
 if [ "$1" = "remove" ]; then
         if [ -x "/usr/bin/deb-systemd-helper" ]; then
-                deb-systemd-helper mask 'consul-etcddiscovery.service' >/dev/null || true
+                deb-systemd-helper mask 'coredhcp.service' >/dev/null || true
         fi
 fi
 
 if [ "$1" = "purge" ]; then
         if [ -x "/usr/bin/deb-systemd-helper" ]; then
-                deb-systemd-helper purge 'consul-etcddiscovery.service' >/dev/null || true
-                deb-systemd-helper unmask 'consul-etcddiscovery.service' >/dev/null || true
+                deb-systemd-helper purge 'coredhcp.service' >/dev/null || true
+                deb-systemd-helper unmask 'coredhcp.service' >/dev/null || true
         fi
 fi
